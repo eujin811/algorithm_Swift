c3621c5eec5655e514f9ec576327246ed9ea83f6
WeatherApp_iOS <eujin8@gmail.com>
2020-05-17 17:41:32 +0900

    Stack

--- study/README.md
+++ study/README.md
@@ -13,8 +13,103 @@
 - push: 데이터 넣을 때
 - pop: 데이터 꺼낼 때
 - top: 가장 마지막에 넣은 데이터
+-    
+   ```swift
+    // 1. 간단 버전
+	class Stack {
+	  private var array = [Int]()
 
+	  init(size: Int) { }
 
+	  func push(_ num: Int) {
+	  	array.append(num)
+	  }
+	  
+	  func pop() -> Int {
+		return array.isEmpty ? -1 : array.removeLast()
+		//return array.popLast()
+	  }
+	 
+	  func size() -> Int {
+		return array.count
+	  }
+
+	  func isEmpty() -> Int {
+		return array.isEmpty ? 1 : 0
+	  }
+
+	  func top() -> Int {
+		return array.last ?? -1
+	  }
+	}	
+   ```
+
+   ```swift
+	//2.
+	class Stack {
+	  private var cArray: [Int}
+	  private var head = 0
+
+	  init(size: Int) {
+		self.cArray = Array<Int>(repeating: 0, count: size)
+	  }
+
+	  func push(_ num: Int) {
+		cArray[head] = num
+		head += 1
+	  }
+
+	  func pop() -> Int {
+		if head == 0 {
+		  	return -1
+		} else {
+			let pop = cArray[head]
+			head -= 1
+			return pop
+		}
+	  }
+
+	  func size() -> Int {
+		return head
+	  }
+
+	  func isEmpty() -> Int {
+		if head == 0 {
+			return 1
+
+		} else {
+			return 0
+		}
+	  }
+	  func top() -> Int {
+		guard head != 0 else { return -1 }
+		return cArray[head - 1]
+	  }
+	}
+
+	let myStack = Stack(size: 1000)
+	
+	func soluton(_ s: String) -> Int {
+	  let comp = s.components(separatedBy: " ")
+
+	  switch comp[0] {
+	     case "push":
+        	guard let value = Int(comp[1]) else { return -1 }
+        	myStack.push(value)
+       	 	return value
+    	     case "pop":
+        	return myStack.pop()
+    	     case "size":
+        	return myStack.size()
+    	     case "empty":
+        	return myStack.isEmpty()
+    	     case "top":
+        	return myStack.top()
+    	     default:
+        	return -1
+	  }
+	}
+   ```
 ## Queue
 - FIFO(선입선출)
 - BFS 알고리즘에서 활용
--- study/Stack_study.playground/Contents.swift
+++ study/Stack_study.playground/Contents.swift
@@ -3,14 +3,95 @@ import UIKit
 import Foundation
 
 /*
+ push X: 정수 X를 스택에 넣는 연산이다.
  pop: 스택에서 가장 위에 있는 정수를 빼고, 그 수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
  size: 스택에 들어있는 정수의 개수를 출력한다.
  empty: 스택이 비어있으면 1, 아니면 0을 출력한다.
  top: 스택의 가장 위에 있는 정수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
  */
 
-func solution(_ s: String) -> String {
-  return ""
+
+class GenericStack<T> {
+    private var array = [T]()
+    
+    func push(_ value: T) {
+        array.append(value)
+    }
+}
+
+class Stack {
+    private var cArray: [Int]
+    private var head = 0
+    
+//    private var array = [Int]()
+    
+    init(size: Int) {
+        self.cArray = Array<Int>(repeating: 0, count: size)
+    }
+    
+    func push(_ num: Int) {
+        cArray[head] = num
+        head += 1
+        
+        //        array.append(num)
+    }
+    
+    func pop() -> Int {
+        if head == 0 {
+            return -1
+        } else {
+            let pop = cArray[head]
+            head -= 1
+            return pop
+        }
+        //return array.isEmpty ? -1 : array.removeLast()
+        //array.popLast()
+    }
+    
+    func size() -> Int {
+        return head
+        //return array.count
+    }
+    
+    func isEmpty() -> Int {
+        if head == 0 {
+            return 1
+        } else {
+            return 0
+        }
+        // return array.isEmpty ? 1 : 0
+    }
+    
+    func top() -> Int {
+        guard head != 0 else { return -1 }
+        return cArray[head - 1]
+        //return array.last ?? -1
+    }
+}
+
+let myStack = Stack(size: 1000)
+
+func solution(_ s: String) -> Int {
+    
+    let comp = s.components(separatedBy: " ")
+    
+    switch comp[0] {
+    case "push":
+        guard let value = Int(comp[1]) else { return -1 }
+        myStack.push(value)
+        return value
+    case "pop":
+        return myStack.pop()
+    case "size":
+        return myStack.size()
+    case "empty":
+        return myStack.isEmpty()
+    case "top":
+        return myStack.top()
+    default:
+        return -1
+    }
+    
 }
 
 let inputs = [
@@ -31,56 +112,6 @@ let inputs = [
 ]
 inputs.forEach { print(solution($0)) }
 
-
-final class Stack {
-//    var arr = [String]()
-//    var head = 0
-    
-    private var storage = [Any]()
-    
-//    init() {
-//        self.array = Array<Int>(repeating: 0, count: size)
-//    }
-    
-    func isEmpty() -> Bool {
-        storage.isEmpty
-    }
-    
-    func pop() -> Any {
-        return storage.popLast() as? String ?? -1
-        //return arrayisEmpty ? -1 : array.removeLast()
-        
-        /*
-         let pop = array[head]
-         head -= 1
-         if head == 0  { return -1 }
-         else {
-            let pop = arr[head]
-            head -= 1
-            return pop
-         }
-         */
-    }
-    
-    func push(content: Any) {
-        storage.append(content)
-        
-        //크기가 지정된 경우
-        /*
-         array[head] = n
-         head += 1
-         */
-    }
-    
-    func size() -> Int {
-        return storage.count
-        
-        /*
-         return head
-         */
-    }
-    
-    
-    
-}
-
+print("--------- 2번 ---------")
+let input2 = ["pop", "top", "push 123", "top", "pop", "top", "pop"]
+input2.forEach { print(solution($0)) }
